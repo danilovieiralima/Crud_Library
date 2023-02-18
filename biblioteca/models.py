0bffe18334f3ade45270a1bd9ae33ca5a93903fe
danilovieiralima <limadanilovieira@gmail.com>
2023-02-18 18:01:36 -0300

    Segundo Commit

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Livros(models.Model):
-    isbn = models.CharField(max_length=13)
+    isbn = models.CharField(max_length=13, unique=True)
     autor = models.CharField(max_length=30)
     titulo = models.CharField(max_length=100)
     descricao = models.TextField()
